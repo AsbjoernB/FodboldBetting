70ea2457ce602967f1e7dc794cbd0db4795a44bd
PapaSchnoof <eliaskristensen1@gmail.com>
2021-01-14 15:09:13 +0100

    Teampoints & Odds ide

--- p5/Match.js
+++ p5/Match.js
@@ -7,7 +7,7 @@ class Match
 		this.day = day;
 		this.month = month;
 		this.year = year;
-		this.homegoals = hour;
+		this.hour = hour;
 		this.minute = minute;
 		this.hometeam = hometeam;
 		this.awayteam = awayteam;
---
+++ p5/Odds.js
@@ -0,0 +1,16 @@
+class Odds{
+constructor(a, b)
+{
+	this.a_points;
+	this.b_points;
+	this.Odds;
+}
+a_points = a;
+b_points = b;
+
+function Odds()
+{
+
+}
+
+}
