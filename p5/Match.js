a252ad197dee09c0d09e7c4bbf4027c861e16cc5
Asbjørn B. Birkelund <asbjoern.birkelund@gmail.com>
2021-01-14 13:41:36 +0100

    Tilføjede Match og MatchDatabase

---
+++ p5/Match.js
@@ -0,0 +1,17 @@
+class Match
+{
+	public Match(round, weekday, day, month, year, hour, minute, hometeam, awayteam, homegoals, awaygoals, spectatorcount)
+	{
+		this.round = round;
+		this.weekday = weekday;
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.homegoals = hour;
+		this.minute = minute;
+		this.hometeam = hometeam;
+		this.awayteam = awayteam;
+		this.homegoals = homegoals;
+		this.awaygoals = awaygoals;
+	}
+}
