f3016d5bc91fc8831d5434b0333e57cb98721191
Asbjørn B. Birkelund <asbjoern.birkelund@gmail.com>
2021-01-15 08:55:07 +0100

    Revert to processing

--- p5/MatchDatabase.js
+++
@@ -1,9 +0,0 @@
-class MatchDatabase
-{
-	static loadMatches()
-	{
-		MatchDatabase.allMatches = [];
-		let result = loadStrings('data/results.txt');
-		//append(MatchDatabase.allMatches, "hej");
-	}
-}
